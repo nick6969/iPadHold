cf489e3ba503483214b31e595fc38a64a1212cd1
Nick <hawhaw.ya@gmail.com>
2022-02-17 12:13:59 +0800

    add First Case `orientationSelected`

---
+++ iPadHold/MainVC.swift
@@ -0,0 +1,78 @@
+//
+//  MainVC.swift
+//  iPadHold
+//
+//  Created by CHIH HAO LIN on 2022/2/17.
+//
+
+import UIKit
+
+enum TalkType: Int, CaseIterable {
+    case orientationSelected = 1000
+}
+
+private extension TalkType {
+    var title: String {
+        switch self {
+        case .orientationSelected:
+            return "轉向方向設定不明確"
+        }
+    }
+}
+
+final class MainVC: UIViewController {
+    
+    private lazy var stackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 20
+        stackView.distribution = .fillEqually
+        for type in TalkType.allCases {
+            stackView.addArrangedSubview(getButton(type))
+        }
+        return stackView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+}
+
+extension MainVC {
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        view.addSubview(stackView)
+        stackView
+            .mLayChainCenterXY()
+            .mLayChain(.width, 300)
+    }
+
+    private func getButton(_ type: TalkType) -> UIButton {
+        let button: UIButton = UIButton()
+        button.setTitle(type.title, for: .normal)
+        button.addTarget(self, action: #selector(didTap(type:)), for: .touchUpInside)
+        button.backgroundColor = .red
+        button.mLayChain(size: CGSize(width: 300, height: 100))
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 24
+        button.tag = type.rawValue
+        return button
+    }
+    
+}
+
+extension MainVC {
+    
+    @objc
+    private func didTap(type button: UIButton) {
+        guard let type = TalkType(rawValue: button.tag) else { return }
+        switch type {
+        case .orientationSelected: break
+        }
+    }
+
+}
