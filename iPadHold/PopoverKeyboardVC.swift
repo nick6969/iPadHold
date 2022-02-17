bf508c7b7130c58b971fe2f504139fcccbd3cb05
Nick <hawhaw.ya@gmail.com>
2022-02-17 13:02:58 +0800

    add note for popoverKeyboardSizeIssue Case

--- iPadHold/PopoverKeyboardVC.swift
+++ iPadHold/PopoverKeyboardVC.swift
@@ -10,7 +10,16 @@ import UIKit
 final class PopoverKeyboardVC: UIViewController {
     
     private let textField: UITextField = UITextField()
-    
+    // ref: https://developer.apple.com/documentation/uikit/uipopoverpresentationcontrollerdelegate/1622326-popoverpresentationcontroller
+    private let noteLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.text = "如果遇到這個問題，就只能請設計這個畫面不要用 popover 的，或者是自定義 inputView"
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -19,9 +28,14 @@ final class PopoverKeyboardVC: UIViewController {
     private func setupUI() {
         view.backgroundColor = .purple
         view.addSubview(textField)
+        view.addSubview(noteLabel)
         textField
             .mLayChainCenterXY()
             .mLayChain(size: CGSize(width: 200, height: 100))
             .backgroundColor = .green
+        noteLabel
+            .mLayChain(.width, .equal, textField)
+            .mLayChain(.centerX, .equal, textField)
+            .mLayChain(.bottom, .equal, textField, .top, constant: -8)
     }
 }
