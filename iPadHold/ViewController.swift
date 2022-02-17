c40d8acdaa9aed6f80e9d4f4b8bf575eaef6da69
Nick <hawhaw.ya@gmail.com>
2022-02-17 11:32:36 +0800

    Initial Commit

---
+++ iPadHold/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iPadHold
+//
+//  Created by CHIH HAO LIN on 2022/2/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
